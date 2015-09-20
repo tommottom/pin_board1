4b4d7bd9644ac138c674aa5c19e99db7b2996386
tommottom <fb53ice4j@gmail.com>
2015-09-20 14:17:56 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash.now[:success] = "Welcome to the Pin Board!"
+      redirect_to root_path #@user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 PinBoard1::Application.routes.draw do
+  resources :users
   resources :pins
-
 
   get '/signup' => 'users#new'
   root 'pins#index'
