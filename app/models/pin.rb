5debf98846ba530a43a6bd721ae8d0f00097e1cf
tommottom <fb53ice4j@gmail.com>
2015-09-21 20:43:48 +0900

    add gem paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,7 @@
 class Pin < ActiveRecord::Base
+  belongs_to :user
   validates :title, presence: true
   validates :description, presence: true, length: { maximum: 1000}
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150>"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
