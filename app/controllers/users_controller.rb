766e61dede0457a841767339d007b188ff7027bf
tommottom <fb53ice4j@gmail.com>
2015-09-22 15:21:34 +0900

    editform & 認可/authorization added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update ]
+  before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -19,9 +21,32 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+   @user = User.find(params[:id])
+  end
+
+
+  def update
+    if @user.update_attributes(user_params)
+      flash.now[:success] = "Profile updated"
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def signed_in_user
+      redirect_to sign_in_path, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 end
